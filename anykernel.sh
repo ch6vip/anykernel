f466b6596752f151764b83e4b5f9a708a702e46d
Stephen Xian <stevexian56@gmail.com>
2020-08-19 14:07:11 +0800

    Init commit
    Signed-off-by: Stephen Xian <stevexian56@gmail.com>

---
+++ anykernel.sh
@@ -0,0 +1,101 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=MARISAKERNEL @ Laulan56
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=cepheus
+device.name2=raphael
+device.name3=raphaelin
+supported.versions=10
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+
+## AnyKernel install
+dump_boot;
+
+
+# end ramdisk changes
+
+
+Marisa=/data/adb/modules/Marisa
+rm -rf $Marisa
+
+case "$ZIPFILE" in
+  *LOSFOD*)
+    ui_print "  • LOSFOD Detected";
+    ;;
+  *MIUI*)
+    ui_print "  • Disabling the FOD Dimlayer";
+    MarisaMagisk=/data/adb/modules/FDD
+    rm -rf $MarisaMagisk
+    mkdir -p $MarisaMagisk
+    cp -Rf /tmp/anykernel/fod_dimlayer_disabler/* $MarisaMagisk
+    chmod 755 $MarisaMagisk/system.prop
+    ;;
+  *)
+    ui_print "  • Disabling the FOD Dimlayer";
+    MarisaMagisk=/data/adb/modules/FDD
+    rm -rf $MarisaMagisk
+    mkdir -p $MarisaMagisk
+    cp -Rf /tmp/anykernel/fod_dimlayer_disabler/* $MarisaMagisk
+    chmod 755 $MarisaMagisk/system.prop
+    ;;
+esac
+
+case "$ZIPFILE" in
+  *66fps*|*66hz*)
+    ui_print "  • Setting 66 Hz refresh rate"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=1"
+    ;;
+  *69fps*|*69hz*)
+    ui_print "  • Setting 69 Hz refresh rate"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=2"
+    ;;
+  *72fps*|*72hz*)
+    ui_print "  • Setting 72 Hz refresh rate"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=3"
+    ;;
+  *75fps*|*75hz*)
+    ui_print "  • Setting 75 Hz refresh rate"
+    patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=4"
+    ;;
+  *)
+    patch_cmdline "msm_drm.framerate_override" ""
+    fr=$(cat /sdcard/framerate_override | tr -cd "[0-9]");
+    [ $fr -eq 66 ] && ui_print "  • Setting 66 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=1"
+    [ $fr -eq 69 ] && ui_print "  • Setting 69 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=2"
+    [ $fr -eq 72 ] && ui_print "  • Setting 72 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=3"
+    [ $fr -eq 75 ] && ui_print "  • Setting 75 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=4"
+    ;;
+esac
+
+insert_line /vendor/etc/fstab.qcom "f2fs" after "/data" "/dev/block/bootdevice/by-name/userdata                  /data                    f2fs    noatime,nosuid,nodev,discard,fsync_mode=nobarrier    latemount,wait,fileencryption=ice,wrappedkey,check,quota,formattable,reservedsize=128M,sysfs_path=/sys/devices/platform/soc/1d84000.ufshc,checkpoint=fs";
+mount -o ro,remount -t auto /vendor;
+
+write_boot;
+## end install
+
